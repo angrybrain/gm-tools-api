b484d94dbcc544e5e26b3e539e4039e7127262c0
angrybrain <56646202+angrybrain@users.noreply.github.com>
2020-05-16 17:18:05 +0300

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.render('index', { title: 'O_o' });
+  res.status(200).json({ "hello": "world" });
 });
 
 module.exports = router;
